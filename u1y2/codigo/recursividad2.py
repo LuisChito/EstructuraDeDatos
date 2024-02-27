ccf2c6c367bcf17a792e531da5d1b60eb9aabb90
LuisChito <lssalazarcanche1@gmail.com>
2024-02-27 16:53:10 -0600

    Varios cambios wn

---
+++ u1y2/codigo/recursividad2.py
@@ -0,0 +1,9 @@
+def suma_lista(lista):
+    if len(lista) == 0:
+        return 0
+    else:
+        return lista[0] + suma_lista(lista[1:])
+
+mi_lista = [1, 2, 3, 4, 5]
+resultado = suma_lista(mi_lista)
+print(f"La suma de los elementos de la lista es: {resultado}")
